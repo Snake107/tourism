62b103491179f63f3b12a661a8a63051151de634
Snake107 <501159870@qq.com>
2019-07-23 09:00:19 +0800

    7.23,controller建立

---
+++ src/main/java/com/ssm/controller/BuycarController.java
@@ -0,0 +1,9 @@
+package com.ssm.controller;
+
+/**
+ * @version 1.0.0
+ * @auther Snake
+ * @date 2019/7/23 8:58
+ */
+public class BuycarController {
+}
---
+++ src/main/java/com/ssm/controller/WishController.java
@@ -0,0 +1,9 @@
+package com.ssm.controller;
+
+/**
+ * @version 1.0.0
+ * @auther Snake
+ * @date 2019/7/23 8:57
+ */
+public class WishController {
+}
